80ad64b9a1dec7902e7f6d6cc3a6dfae9335a335
Colax13 <126708096+Colax13@users.noreply.github.com>
2026-04-07 19:41:33 +0200

    refactor: Update entry point and remove unused FormProvider
    Moves the application entry point to `src/main.tsx` and updates `index.html` accordingly. Also, removes the unused `FormProvider` from the root component tree.

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { FormProvider } from './context/FormContext.tsx';
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <FormProvider>
-        <App />
-      </FormProvider>
+      <App />
     </BrowserRouter>
   </StrictMode>,
 );
